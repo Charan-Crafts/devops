6d888bdfa29038b26e1891025a8baed0bcfe0066
charan-crafts <mangalacharancharan@gmail.com>
2025-06-02 23:37:27 +0530

    These is Developer 1 added the Signup page for the Company

---
+++ App/Feature-1/feature1.py
@@ -0,0 +1,5 @@
+print("--------------Feature 1 ---------------")
+
+print("Hello User welcome to the AI AUTOMAGIX these is Your Signup Page")
+
+print("These code is written By Developer -1 from the Feature 1 Branch")
