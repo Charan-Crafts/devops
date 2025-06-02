b75bdcba305274c9e17d9e82b14419d26656c17f
charan-crafts <mangalacharancharan@gmail.com>
2025-06-02 23:41:18 +0530

    Added the Login Page in the Feature1

--- App/Feature-1/feature1.py
+++ App/Feature-1/feature1.py
@@ -3,3 +3,7 @@ print("--------------Feature 1 ---------------")
 print("Hello User welcome to the AI AUTOMAGIX these is Your Signup Page")
 
 print("These code is written By Developer -1 from the Feature 1 Branch")
+
+print("Hello user welcome to the AI AUTOMAGIX these is Your Login Page")
+
+print("These code is written by Developer -2 from the Feature 1 Branch")
